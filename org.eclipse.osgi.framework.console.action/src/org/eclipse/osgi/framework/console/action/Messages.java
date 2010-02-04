62b5f01f86692623a5d6db4cb57e477f6f5fafee
Philipp Kursawe <phil.kursawe@gmail.com>
2010-02-04 16:11:09 +0100

    Changed

--- org.eclipse.osgi.framework.console.action/src/org/eclipse/osgi/framework/console/action/Messages.java
+++ org.eclipse.osgi.framework.console.action/src/org/eclipse/osgi/framework/console/action/Messages.java
@@ -2,13 +2,20 @@ package org.eclipse.osgi.framework.console.action;
 
 import org.eclipse.osgi.util.NLS;
 
+/***/
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.osgi.framework.console.action.messages"; //$NON-NLS-1$
+	/***/
 	public static String ActionCommand_DidYouMean;
+	/***/
 	public static String ActionCommand_Examples;
+	/***/
 	public static String ActionCommand_ForHelpEnter;
+	/***/
 	public static String ActionCommand_NoActions;
+	/***/
 	public static String ActionCommand_NoHelp;
+	/***/
 	public static String ActionCommand_UnknownAction;
 	static {
 		// initialize resource bundle
